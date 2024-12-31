5ce6e65ec697f518b97c6cfeb791694ced4651d1
IM94 <gladiator.pw@gmail.com>
2024-12-31 12:25:45 +0300

    Added message_assistant

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from services.open_ai_service import OpenAIService
 from services.audio import Audio
 
 
-if __name__ == '__main__':
-
+def audio_assistant():
     _audio = Audio()
     _openai = OpenAIService()
-
 
     print("Привет! Вы можете задать вопрос голосом.")
     _developer_assistant_context = ("You are a helpful assistant that answers programming questions "
@@ -15,6 +13,7 @@ if __name__ == '__main__':
     while True:
         # Голосовой ввод
         question = _audio.speech_to_text()
+
         if question.lower() in ["выход", "стоп", "закончить"]:
             print("Завершение работы.")
             break
@@ -31,3 +30,34 @@ if __name__ == '__main__':
             _audio.text_to_speech(response_text)
         except Exception as e:
             print(f"Ошибка при обработке запроса: {e}")
+
+def message_assistant():
+    _openai = OpenAIService()
+
+    while True:
+
+        _developer_assistant_context = ("You are a helpful assistant that answers programming questions "
+                                        "in the style of a southern belle from the southeast United States.")
+
+        question = input("Привет! Вы можете задать вопрос отправив текстовое сообщение ниже:")
+
+        if question.lower() in ["выход", "стоп", "закончить"]:
+            print("Завершение работы.")
+            break
+
+        print(f"Ваш вопрос: {question}")
+
+        try:
+            response_text = _openai.developer_type_assistant(question_text=question,
+                                                             content=_developer_assistant_context)
+            print(f"Ответ: {response_text}")
+
+        except Exception as e:
+            print(f"Ошибка при обработке запроса: {e}")
+
+
+
+if __name__ == '__main__':
+
+    # audio_assistant()
+    message_assistant()
